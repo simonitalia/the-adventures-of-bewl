6202f1994ce78b3ad7aebabd492a97e8b83558e3
Simon Italia <simonitaliaatwork@gmail.com>
2021-11-17 16:38:57 +0100

    Initial Commit

---
+++ new-owl-adventures/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  new-owl-adventures
+//
+//  Created by Simon Italia on 17/11/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ new-owl-adventures/new_owl_adventuresApp.swift
@@ -0,0 +1,17 @@
+//
+//  new_owl_adventuresApp.swift
+//  new-owl-adventures
+//
+//  Created by Simon Italia on 17/11/21.
+//
+
+import SwiftUI
+
+@main
+struct new_owl_adventuresApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
