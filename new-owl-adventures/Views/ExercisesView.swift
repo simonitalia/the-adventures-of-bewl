73cf824b8a7968e145d17048c08f9116ddac0722
Simon Italia <simonitaliaatwork@gmail.com>
2021-11-18 00:00:56 +0100

    add initial files for exercises and awards views to build tab bar view

---
+++ new-owl-adventures/Views/ExercisesView.swift
@@ -0,0 +1,20 @@
+//
+//  ExercisesView.swift
+//  new-owl-adventures
+//
+//  Created by Simon Italia on 17/11/21.
+//
+
+import SwiftUI
+
+struct ExercisesView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ExercisesView_Previews: PreviewProvider {
+    static var previews: some View {
+        ExercisesView()
+    }
+}
