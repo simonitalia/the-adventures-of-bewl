40d7246df0ecbc132fbe399dfa6fc9a4a3042a99
Simon Italia <simonitaliaatwork@gmail.com>
2021-11-20 18:03:32 +0100

    - remove filter view - remove filter logic

--- new-owl-adventures/Views/AdventuresView.swift
+++ new-owl-adventures/Views/AdventuresView.swift
@@ -12,7 +12,7 @@ struct AdventuresView: View {
     private let adventures = Adventures().list
     
     @State private var searchText = ""
-    @State private var selectedMood = Adventure.Tags.all
+    @State private var selectedMood = ""
     @State private var isSearching = false
     
     var body: some View {
@@ -20,36 +20,23 @@ struct AdventuresView: View {
         let columns = [GridItem(.flexible())]
         
         NavigationView {
-            VStack(alignment: .center, spacing: 5) {
-                FilterView(selectedMood: $selectedMood)
-                
-                ScrollView {
-                    LazyVGrid(columns: columns, alignment: .center, spacing: 15) {
-                        ForEach(adventures) { adventure in
-                            
-//                            if adventure.moods.contains(selectedMood.rawValue)
-//                                && (adventure.name.localizedCaseInsensitiveContains(searchText)
-//                                || adventure.genre.localizedCaseInsensitiveContains(searchText)
-//                                    || adventure.moods.map({ $0.localizedCaseInsensitiveContains(searchText)}).contains(true)
-//                                || searchText == "") {
-//                                NavigationLink(destination: Text("AdventureView")) {
-                                AdventureItemView(adventure: adventure)
-//                                }
-//                            }
-                        }
+            ScrollView {
+                LazyVGrid(columns: columns, alignment: .center, spacing: 15) {
+                    ForEach(adventures) { adventure in
+                            AdventureItemView(adventure: adventure)
                     }
-                    .padding(.horizontal)
                 }
-                .searchable(text: $searchText)
-                .navigationTitle("Adventures")
-                .gesture(
-                    DragGesture()
-                        .onChanged({ _ in
-                            UIApplication.shared.dismissKeyboard()
-                        })
-                    )
+                .padding(.horizontal)
             }
             .background(Color(AppTheme.primaryBackgroundColor))
+            .searchable(text: $searchText)
+            .navigationTitle("Adventures")
+    //            .gesture(
+    //                DragGesture()
+    //                    .onChanged({ _ in
+    //                        UIApplication.shared.dismissKeyboard()
+    //                    })
+    //                )
         }
         .onAppear {
             // configure navigation view appearance
