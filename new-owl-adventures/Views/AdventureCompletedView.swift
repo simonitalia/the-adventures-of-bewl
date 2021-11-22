5046d4e287f96d24e15e79a31ab50d664412dafd
Simon Italia <simonitaliaatwork@gmail.com>
2021-11-22 03:18:12 +0100

    build out adventure completed view

--- new-owl-adventures/Views/AdventureCompletedView.swift
+++ new-owl-adventures/Views/AdventureCompletedView.swift
@@ -12,11 +12,45 @@ struct AdventureCompletedView: View {
 
     let adventure: Adventure
     
+    var completedAdventureText: String {
+        
+        switch adventure.name {
+        case .classical:
+            return "We did amazing team work!\nWe have collected a DO note!"
+            
+        case .nature:
+            return "We did amazing team work!\nThe wizard cooked the potion!"
+            
+        case .cinematic:
+            return "We did amazing team work!\nBewl was cast as the leading Ownl!"
+        
+        }
+    }
+    
     var body: some View {
         NavigationView {
-            ZStack {
+            ZStack(alignment: .top) {
                 Color(AppTheme.primaryBackgroundColor).edgesIgnoringSafeArea(.all)
+                
+                Image(adventure.detailImage)
+                    .resizable()
+                    .scaledToFill()
+                    .overlay() {
+                        Color.black.opacity(0.1)
+                    }
+                
+                VStack(alignment: .center) {
+                    Text(completedAdventureText)
+                        .font(.system(size: 18, weight: .bold, design: .serif))
+                        .foregroundColor(Color(AppTheme.primaryForegroundColor))
+                        .overlay(RoundedRectangle(cornerRadius: 20)
+                                    .frame(width: 400, height: 100, alignment: .center)
+                                    .opacity(0.3)
+                        )
+                }
+                .padding(40)
             }
+            .navigationBarTitleDisplayMode(.inline) //small navigation bar
             .toolbar {
                 Button("Done") {
                     PresentationMode.wrappedValue.dismiss()
